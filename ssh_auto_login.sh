e327d33b382d925fade9ca696c84b1becea3f239
dxdbl <mhf3@163.com>
2019-05-15 16:21:58 +0800

    添加注释

---
+++ ssh_auto_login.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# ssh_auto_login.sh
+#
+# @base     expect
+# @author   dxdbl
+# @link     https://github.com/dxdbl/ssh_auto_login
+#
+# $ git clone https://github.com/dxdbl/ssh_auto_login.git
+# $ 
+# $ 
+#
